1044cee7001c288b2288906a17559383f4279ea5
StanBorovko <34895740+StanBorovko@users.noreply.github.com>
2019-09-10 13:24:03 +0300

    Main.js refactoring: move all solving logic to class App

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ class Slot {
     }
 }
 
-const slot_a = new Slot('slot_a'),
+/*const slot_a = new Slot('slot_a'),
     slot_b = new Slot('slot_b'),
     slot_c = new Slot('slot_c');
 
@@ -46,5 +46,44 @@ solver(3, slot_a, slot_c, slot_b);
 
 console.log('slot_a', slot_a.getContent());
 console.log('slot_b', slot_b.getContent());
-console.log('slot_c', slot_c.getContent());
+console.log('slot_c', slot_c.getContent());*/
 
+class App {
+    constructor(platesQuatity) {
+        this.slot_a = new Slot('slot_a');
+        this.slot_b = new Slot('slot_b');
+        this.slot_c = new Slot('slot_c');
+        this.platesQuatity = platesQuatity;
+    }
+    solve(n, source, target, auxiliary) {
+        if (n > 0) {
+            this.solve(n-1, source, auxiliary, target);
+            let cargo = source.unload();
+            target.load(cargo);
+            this.solution += `#${cargo.id} ${source.getName()} -> ${target.getName()}` + '\n';
+            // console.log(source.getName(), '->', target.getName(), source.getName(), source.getContent(), target.getName(), target.getContent() );
+            this.solve(n - 1, auxiliary, target, source);
+        }
+    }
+    run() {
+        if (this.platesQuatity < 3 || this.platesQuatity > 8) {
+            return `Wrong plates quantity! It should be in range from 3 to 8. Not ${this.platesQuatity}.`
+        }
+        for (let i = this.platesQuatity; i > 0; i--) {
+            this.slot_a.load({id: i});
+        }
+        this.solution = '';
+        this.solve(this.platesQuatity, this.slot_a, this.slot_c, this.slot_b);
+        return this.solution
+    }
+}
+
+const app1 = new App(1);
+const app2 = new App(10);
+const app3 = new App(3);
+const app4 = new App(8);
+
+console.log(app1.run());
+console.log(app2.run());
+console.log(app3.run());
+console.log(app4.run());
